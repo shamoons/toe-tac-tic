92455bb6c838d3d61dd45159d209cf5bd6e15fe5
Christian Eckenrode Work <christianeckenrodework@Christians-MBP.home>
2016-05-21 19:27:31 -0400

    Started logic for determining wins

--- js/controllers/localGame.js
+++ js/controllers/localGame.js
@@ -29,5 +29,26 @@ angular.module('toeTactic').controller('LocalGameCtrl', function($scope) {
       $scope.turn = "X"
     }
   }
+  $scope.checkWin = function() {
+    var winMatrix = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    var currentWinner = null;
+    _.each(winMatrix, function(winPath) {
+      if(currentWinner === null) {
+        if($scope.gameMap[winPath[0]].value === $scope.gameMap[winPath[1]].value && $scope.gameMap[winPath[1]].value === $scope.gameMap[winPath[2]].value) {
+          currentWinner = $scope.gameMap[winPath[0]].value;
+          alert('winner is ' + currentWinner);
+        }
+      }
+    });
+  }
 
 });
